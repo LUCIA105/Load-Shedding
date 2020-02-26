94082251ee868058902024f10f6a578765d8b6e1
LUCIA105 <61186388+LUCIA105@users.noreply.github.com>
2020-02-26 11:18:18 +0200

    Add files via upload

---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+using System.Web.WebPages;
+
+namespace LoadsheddingSystem
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+
+           AreaRegistration.RegisterAllAreas();
+
+            var displayModes = DisplayModeProvider.Instance.Modes;
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
